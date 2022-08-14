c359906536ca43b67fdd788f85811e97d4b122d0
Jiří Masař <jiri-masar@centrum.cz>
2022-08-14 13:55:17 +0200

    Initial commit

---
+++ src/main/java/com/example/springtest/exceptions/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.springtest.exceptions;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException(String message){
+        super(message);
+    }
+
+
+}
